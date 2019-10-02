5567627d7a14ec8f22c2c8d56ff10daa7b3a8802
dipasquale <dario.dipasquale@immobiliare.it>
2019-10-02 13:29:53 +0200

    0x2 solution

--- 0x2/oo.go
+++ 0x2/oo.go
@@ -1,4 +1,9 @@
-package _x2
+package main
+
+import (
+	"fmt"
+	"golearngo/0x2/mine"
+)
 
 // Abbiamo una miniera d'oro (un file contenente o una roccia o un pezzo d'oro grezzo).
 // Abbiamo tre categorie diverse di lavoratori:
@@ -6,4 +11,11 @@ package _x2
 // * il miner: ricevute le posizioni in cui si trova l'oro grezzo, può minarli ottenendo pepite d'oro
 // * il fonditore: riceve le pepite d'oro e le fonde in un unico lingotto
 func main() {
+	minePath := "./0x2/res/mine"
+	m := mine.GiveMyMine(minePath)
+	finder := new(mine.IndolentFinder)
+	miner := new(mine.IndolentMiner)
+	smelter := new(mine.IndustriousSmelter)
+	ingot := m.WorkSequential(finder, miner, smelter)
+	fmt.Println(ingot)
 }
